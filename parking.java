ac9baebfa4acf4b9ca87fedc954dba0094134110
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-13 00:04:17 +0530

    Add files via upload

--- parking.java
+++ parking.java
@@ -1,50 +1,84 @@
 //Program to demonstrate the operations carried out in a parking lot
-
 import java.util.*;
-
-class vehicle {
-
+class vehicle 
+{
     int type;
     twowheeler two;
     fourwheeler four;
-
-    vehicle(int type) {
+    vehicle(int type)
+    {
         this.type = type;
-        if (this.type == 1) {
-            two = new twowheeler();     //Calling a Method from another class 
-        } else {
+        if(this.type == 1) 
+        {
+            two =  new twowheeler();     //Calling a Method from another class 
+        } 
+        else 
+        {
             four = new fourwheeler();  //Calling amethod from another class
         }
     }
 }
-
-class twowheeler {
-
-    twowheeler() {
-        displaydetails();
-    }
-
-    public void displaydetails() {
+class twowheeler 
+{
+    Scanner sc=new Scanner(System.in);
+    String type;
+    twowheeler() 
+    {
         System.out.println("Please select your Two Wheeler Type");
         System.out.println("TYPE 1---MOTOR BIKE");
         System.out.println("TYPE 2---CYCLE");
+        System.out.println("Your choice: ");
+        String type=sc.nextLine();
+        this.type=type;
+        if(this.type=="motorbike")
+        {
+           new motorbike();
+        }
+        else if(this.type=="cycle")
+        {
+          new cycle();
+        }
     }
 }
+class motorbike
+{
+    
+}
+class cycle
+{
 
-class fourwheeler {
-
-    fourwheeler() {
-        displaydetails();
-    }
-
-    public void displaydetails() {
+}
+class fourwheeler 
+{
+    Scanner sc=new Scanner(System.in);
+    String type;
+    fourwheeler() 
+    {
         System.out.println("Please Select your Four wheeler Type");
         System.out.println("TYPE 1---CAR");
         System.out.println("TYPE 2---VAN");
         System.out.println("TYPE 3---TRUCK");
+        System.out.println("Your choice :" );
+        String type=sc.nextLine();
+        this.type=type;
+        if(this.type=="car")
+        {
+           new car();
+        }
+        else if(this.type=="van")
+        {
+            new van();
+        }
+        else if(this.type=="truck")
+        {
+            new truck();
+        }
+        else 
+        {
+            System.out.println("Invalid Input");
+        }
     }
 }
-
 class parkinglot //Class to demonstrate the Number of lots for each type of vehicle
 {
 
@@ -60,43 +94,49 @@ class parkinglot //Class to demonstrate the Number of lots for each type of vehi
     int vspots = 10;
     int tspots = 5;
 
-    parkinglot(String type) {
-        if (this.type == "motorbike") {
+    parkinglot(String type) 
+    {
+        if (this.type == "motorbike") 
+        {
             System.out.println("No of Parking lots Available for Motor Bikes are- " + this.mbspots);
             mb.fare();
-        } else if (this.type == "cycle") {
+        } else if (this.type == "cycle") 
+        {
             System.out.println("No of Parking Lots Available for Cycles are- " + this.cyspots);
             cy.fare();
-        } else if (this.type == "car") {
+        } else if (this.type == "car") 
+        {
             System.out.println("No of Parking Lots Available for Cars are- " + this.clspots);
             cr.fare();
-        } else if (this.type == "van") {
+        } else if (this.type == "van") 
+        {
             System.out.println("No of Parking Lots Available for Vans are- " + this.vspots);
             v.fare();
-        } else if (this.type == "truck") {
+        } else if (this.type == "truck") 
+        {
             System.out.println("No of Parking lots Available for Trucks are- " + this.tspots);
             t.fare();
-        } else {
+        } else 
+        {
             System.out.println("Invalid Input");
         }
     }
 }
-
-interface parkingfare {
-
+interface parkingfare 
+{
     public void fare();
 }
-
-class mbfare implements parkingfare {
-
-    public void fare() {
+class mbfare implements parkingfare 
+{
+    public void fare() 
+    {
         System.out.println("Parking Fare for Motor Cycle");
         System.out.println("1-3hrs --- " + "80");
         System.out.println("More than 3hrs --- " + "130");
     }
 }
-
-class cfare implements parkingfare {
+class cfare implements parkingfare 
+{
 
     public void fare() {
         System.out.println("Parking Fare for Cycle");
@@ -125,24 +165,26 @@ class vfare implements parkingfare {
 
 class tfare implements parkingfare {
 
-    public void fare() {
+    public void fare() 
+    {
         System.out.println("Parking Fare for Trucks");
         System.out.println("1-3hrs --- " + "200");
         System.out.println("More than 3hrs --- " + "250");
     }
 }
 
-class parking {
-
-    public static void main(String args[]) {
+class parking 
+{
+    public static void main(String args[]) 
+    {
         System.out.println("================================");
         System.out.println("PLEASE PARK YOUR VEHICLES HERE");
         Scanner sc = new Scanner(System.in);
         System.out.println("Select your vehicle");
         System.out.println("1.TWO-WHEELER");
         System.out.println("2.FOUR-WHEELER");
-        System.out.print("Your choice: ");
-        int s = sc.nextInt();
+        System.out.println("Your choice: ");
+        int s=sc.nextInt();
         new vehicle(s);
     }
 }

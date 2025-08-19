0dc774e2c18caac0ae6fe61f4cb7f78eeb7c0a8b
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-19 18:17:06 +0530

    Add files via upload

---
+++ Assignments/Big int.js
@@ -0,0 +1,10 @@
+//Program to demonstrate the operations of bigint in javascript
+let x=1283648236343344n;  //bigint integers are always denoted with the suffix n
+let y=BigInt(12412323482384342);
+console.log(x);
+console.log(y);
+console.log("x+y=",x+y);
+console.log("x-y=",x-y);
+console.log("x*y=",x*y);
+console.log("x/y=",x/y);
+console.log("x%y=",x%y);

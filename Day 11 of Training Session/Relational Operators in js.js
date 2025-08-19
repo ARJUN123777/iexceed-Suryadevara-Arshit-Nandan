e784a2211cef242e1fd8ba630fa2948d3a396c0b
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-19 18:16:20 +0530

    Add files via upload

---
+++ Day 11 of Training Session/Relational Operators in js.js
@@ -0,0 +1,14 @@
+//Program to demonstrate the Relational operations in java script
+console.log("AB">"B"); //returns false
+console.log("BA"<="B");   //returns false
+console.log("ABA"<="BA");     //returns true
+console.log("welcome to javaScript">"welcome to javascript");//returns false
+console.log("javascripT"<"javAscript");//returns false
+console.log("javascript"<="scriptjava");//returns true
+console.log("zepto">="zeptohorse");//returns false
+console.log("independenceday"<="republicday"); //returns true
+console.log("123">342); //It converts the string into integer
+console.log("122">="z"); //returns false
+console.log("999"<="9A");//returns true
+console.log("123">"20");//returns false
+console.log("12983.23711">1239);

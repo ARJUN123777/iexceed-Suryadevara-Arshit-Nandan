afaea4b74cdbab18f72b83c858ea8edcf033aa45
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-20 11:29:01 +0530

    Add files via upload

--- Day 10 of Training Session/simplejs.js
+++ Day 10 of Training Session/simplejs.js
@@ -17,3 +17,35 @@ function add(x,y)
   }
 }
 add(10,20);
+function iteration()
+{
+  for(let i=0;i<7;i++)
+  {
+    console.log(i);
+    console.log(delete i); //output will be false
+  }
+  //console.log(i);//It shows an error 
+}
+iteration();
+'use strict';
+var val="true";
+function istrue()
+{
+  if(val)
+  {
+    console.log("The answer is true");
+  }
+  else
+  {
+    console.log("The answer is false");
+  }
+}
+istrue();
+'use strict';
+ function deleteme()
+{
+    var x=1000;
+    console.log(delete x);
+    console.log(x);
+}
+deleteme();

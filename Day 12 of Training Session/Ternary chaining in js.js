9cf2a6703f957e96c5bc80ef1ebb82f464265b36
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-21 17:57:03 +0530

    Add files via upload

--- Day 12 of Training Session/Ternary chaining in js.js
+++ Day 12 of Training Session/Ternary chaining in js.js
@@ -5,11 +5,14 @@ console.log(result);
 function displayresult(mark1)
 {
     let marks=mark1;
-    let result=marks<35?"Fail":marks>35&&marks<=50?"Grade E":marks>50&&marks<=60?"Grade D":marks>60&&marks<=70?"Grade C":marks>70&&marks<=80?"Grade B":marks>80&&marks<=90?"Grade A":marks>90&&marks<=100?"Grade A+":"Please enter your Marks Properly";
+    let result=marks<35?"Fail":marks>35 && marks<=50?"Grade E":marks>50 && marks<=60?"Grade D":marks>60 && marks<=70?"Grade C":marks>70 && marks<=80?"Grade B":marks>80 && marks<=90?"Grade A":marks>90 && marks<=100?"Grade A+":"Please enter your Marks Properly";
     return result;
 }
 console.log(displayresult(58));
-var d1=1334.1
-console.log(d1);
-var d1="mystring"
+var d1=1334.1    //var can be redeclared and reassigned using the same variable
+console.log(d1);  //let can be reassigned but can not be redeclared
+var d1="mystring"   
 console.log(d1)
+//ternary chaining using arrow function
+let v=(volts)=>volts<=300?"Free":volts>=400 && volts<500?"Pay 700Rs":volts>=500 && volts<600?"Pay 800Rs":volts>=600 && volts<700?"Pay 900Rs":volts>=700 && volts<800?"Pay 1000rs":volts>=800 && volts<900?"Pay 1200Rs":"Pay 1500Rs"
+console.log(v(694));
--- Day 12 of Training Session/arrow function.js
+++ Day 12 of Training Session/arrow function.js
@@ -7,5 +7,32 @@ const mail=(x)=>x+"@"+"gmail.com";
 console.log(mail("devansh"));
 let empdetails=(id,empname,depart,gender,salary)=>"id:"+id+"\n"+"name:"+empname+"\n"+"Department:"+depart+"\n"+"Gender:"+gender+"\n"+"Salary:"+salary;
 console.log(empdetails(162,"Vivek","Cyber Security",'M',1600000));
-var stdetails=(id,stname,cl,sec,feesPaid)=>arguments();
-console.log(stdetails(159,'Arjun',10,'M',50000));
+//var stdetails=(id,stname,cl,sec,feesPaid)=>arguments(id,stname,cl,sec,feesPaid);   //We can not use the arguments in the arrow function
+//console.log(stdetails(159,'Arjun',10,'M',50000));
+
+var details=(teachid,teachname,teachsubs,teachdept,teachsal)=>
+{
+    console.log("Teacher id: "+teachid);
+    console.log("Tecaher Name: "+teachname);
+    console.log("Teaching Subjects: "+teachsubs);
+    console.log("Teacher Department: "+teachdept);
+    console.log("Salary: "+teachsal);
+}
+console.log(details(372,'Sri Lakshmi',['Maths','Physics','Chemistry'],'Science',50000));
+
+
+var bdetails=(bdno,bname,barea,bvalue,...args)=>
+    {
+     console.log("Building No: "+bdno);
+     console.log("Building Name: "+bname);
+     console.log("Building Area: "+barea);
+     console.log("Cost of Building: "+bvalue);
+     console.log(args.sort());
+}
+console.log(bdetails('TR122','Suavity Otium','Akshaynagar',2300000,'Bangalore urban','Bangalore','India','Asia',))
+
+var x=(age)=>age>20 && age<=31?"Eligible to Government Exams":age>31 && age<50?"Apply for Ration Card":age>=50 && age<90?"Apply for Pension":"U are not eligible for government schemes"
+console.log(x(29));
+
+
+

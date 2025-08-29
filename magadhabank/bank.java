673871484706ef07e8d6dd2b31fa45ec73e40149
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-08-29 17:45:21 +0530

    Add files via upload

--- magadhabank/bank.java
+++ magadhabank/bank.java
@@ -1,6 +1,8 @@
 //Program to execute the operations carried out in the bank
 package magadhabank;
 import java.util.Scanner;
+import counter1.*;
+import counter2.*;
 import java.util.*;
 public class bank
 {
@@ -10,37 +12,34 @@ public class bank
       int option;
       while(true)
       {
-        System.out.println("============================================");
-        System.out.println("WECLOME TO MAGADHA BANK");
-        System.out.println("HOW CAN WE HELP YOU?");
-        System.out.println("1.CREATE AN ACCOUNT");
-        System.out.println("2.DEPOSIT MONEY INTO YOUR ACCOUNT");
-        System.out.println("3.LOAN");
-        System.out.println("4.Exit");
-        System.out.print("PLEASE SELECT YPOUR CHOICE: ");
+        System.out.println("==========================================================================================");
+        System.out.println("\t\tWECLOME TO MAGADHA BANK");
+        System.out.println("\t\tHOW CAN WE HELP YOU?");
+        System.out.println("\t\t1.CREATE AN ACCOUNT");
+        System.out.println("\t\t2.DEPOSIT MONEY INTO YOUR ACCOUNT");
+        System.out.println("\t\t3.LOAN");
+        System.out.println("\t\t4.Exit");
+        System.out.print("\t\tPLEASE SELECT YOUR CHOICE: ");
         option=sc.nextInt();
         if(option==1)
         {
-            System.out.println("=================================================");
-            System.out.println("FOR ACCOUNT CREATION PLEASE GO TO COUNTER 1");
-            counter1();
+            System.out.println("\t\tFOR ACCOUNT CREATION PLEASE GO TO COUNTER 1");
+            new create();
         }
         else if(option==2)
         {
-            System.out.println("=================================================");
-            System.out.println("FOR DEPOSITING MONEY INTO YOUR ACCOUNT PLEASE GO TO COUNTER 2");
-            counter2();
+            System.out.println("\t\tFOR DEPOSITING MONEY INTO YOUR ACCOUNT PLEASE GO TO COUNTER 2");
+            new deposit();
         }
         else if(option==3)
         {
-            System.out.println("=================================================");
-            System.out.println("FOR LOAN PLEASE GO TO COUNTER 3");
-            counter3();
+            System.out.println("\t\tFOR LOAN PLEASE GO TO COUNTER 3");
+            //counter3();
         }
         else if(option==4)
         {
-            System.out.println("=================================================");
-            System.out.println("THANK YOU FOR BAKING WITH US,PLEASE VISIT AGAIN");
+            System.out.println("==========================================================================================");
+            System.out.println("\t\tTHANK YOU FOR BANKING WITH US,PLEASE VISIT AGAIN");
         }
         else
         {

8b20f3d66bcf84d3eabcee7a102a894ed1719dc8
Arshit Nandan Suryadevara <96990123+ARJUN123777@users.noreply.github.com>
2025-09-02 23:46:05 +0530

    Add files via upload

--- counter1/create.java
+++ counter1/create.java
@@ -3,15 +3,15 @@ import java.io.*;
 import java.util.*;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-public class create
-{
-Scanner sc = new Scanner(System.in);
+
+public class create {
+    Scanner sc = new Scanner(System.in);
     double balance = 0.0;
     String username, pwd;
     String st, aadhar, address;
     int age;
-    public create()
-    {
+
+    public create() {
         System.out.println("\t\t=============================================================");
         System.out.println("\t\t\t\tWELCOME TO COUNTER 1");
         System.out.println("\n\t\tPLEASE FILL THE DETAILS TO CREATE A BANK ACCOUNT");
@@ -20,78 +20,69 @@ Scanner sc = new Scanner(System.in);
         System.out.print("\t\t\tYour AGE: ");
         this.age = sc.nextInt();
         sc.nextLine(); // Consume the newline character
-        if (this.age < 18)
-        {
+        if (this.age < 18) {
             System.out.println("\t\t\t\tYOU ARE NOT ELIGIBLE TO CREATE A BANK ACCOUNT");
-        }
-        else
-        {
-            while (true)
-            {
-               System.out.print("\t\t\tYOUR AADHAR NO: ");
-               this.aadhar = sc.nextLine();
-            if (this.aadhar.length() != 12 || (!this.aadhar.matches("\\d{12}")))
-            {
-                System.out.println("\t\t\tPLEASE ENTER A VALID 12 DIGIT  AADHAR NO");
+        } else {
+            while (true) {
+                System.out.print("\t\t\tYOUR AADHAR NO: ");
+                this.aadhar = sc.nextLine();
+                if (this.aadhar.length() != 12 || (!this.aadhar.matches("\\d{12}"))) {
+                    System.out.println("\t\t\tPLEASE ENTER A VALID 12 DIGIT  AADHAR NO");
+                } else {
+                    break;
+                }
             }
-            else
-            {
-                break;
-            }
-            }
-                System.out.print("\t\t\tENTER YOUR ADDRESS: ");
-                this.address = sc.nextLine();
-                this.username = this.aadhar.substring(this.aadhar.length() - 6);
-                System.out.println("\t\t\tYOUR USERNAME(last 6 digits of your AADHAR number): "+this.username);
-                System.out.print("\t\t\tSET YOUR PASSWORD: ");
-                this.pwd = sc.nextLine();
-                try{
-                       createaccount();
-                }catch(Exception e){
+            System.out.print("\t\t\tENTER YOUR ADDRESS: ");
+            this.address = sc.nextLine();
+            this.username = this.aadhar.substring(this.aadhar.length() - 6);
+            System.out.println("\t\t\tYOUR USERNAME(last 6 digits of your AADHAR number): " + this.username);
+            System.out.print("\t\t\tSET YOUR PASSWORD: ");
+            this.pwd = sc.nextLine();
+            try {
+                createaccount();
+            } catch (Exception e) {
                 System.out.println("\t\t\tERROR WHILE CREATING ACCOUNT.");
-                 e.printStackTrace();
-                } 
+                e.printStackTrace();
+            }
         }
     }
-    public void createaccount() throws Exception
-    {
+
+    public void createaccount() throws Exception {
         try {
             File file = new File(username + ".txt");
-        if (file.exists()) 
-        {
-            System.out.println("\t\t\tACCOUNT ALREADY EXISTS FOR USER " + username);
-            return;
-        } 
-            FileOutputStream fos = new FileOutputStream(file,false);
+            if (file.exists()) {
+                System.out.println("\t\t\tACCOUNT ALREADY EXISTS FOR USER " + username);
+                return;
+            }
+            FileOutputStream fos = new FileOutputStream(file, false);
             String data = "\t\tNAME: " + st + "\n" +
-                          "\t\tAADHAR NO: " + aadhar + "\n" +
-                          "\t\tAGE: " + age + "\n" +
-                          "\t\tADDRESS: " + address + "\n" +
-                          "\t\tBALANCE: " + balance + "\n" ;
+                    "\t\tAADHAR NO: " + aadhar + "\n" +
+                    "\t\tAGE: " + age + "\n" +
+                    "\t\tADDRESS: " + address + "\n" +
+                    "\t\tBALANCE: " + balance + "\n";
             byte[] encryptedData = encrypt(data, pwd);
             fos.write(encryptedData);
             fos.close();
             System.out.println("\t\t\tYOU CAN LOGIN USING YOUR USERNAME AND PASSWORD");
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("\t\t\tERROR WHILE CREATING ACCOUNT FILE.");
             e.printStackTrace();
         }
     }
-    public static byte[] encrypt(String data, String password) throws Exception
-    {
+
+    public static byte[] encrypt(String data, String password) throws Exception {
         SecretKeySpec key = new SecretKeySpec(getKey(password), "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return cipher.doFinal(data.getBytes());
     }
-    public static byte[] getKey(String password) throws Exception
-    {
+
+    public static byte[] getKey(String password) throws Exception {
         byte[] key = password.getBytes("UTF-8");
         return Arrays.copyOf(key, 16); // use first 16 bytes
     }
-    public static String decrypt(byte[] data, String password) throws Exception
-    {
+
+    public static String decrypt(byte[] data, String password) throws Exception {
         SecretKeySpec key = new SecretKeySpec(getKey(password), "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, key);
